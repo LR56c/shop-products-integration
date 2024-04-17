7a7b178fd1f2862d6f4b436009b9f7783fbeec96
Mil0 <cam.trivino@gmail.com>
2024-04-17 14:05:24 -0400

    Feat(componentUI): add selector

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,56 @@
 import './App.css'
 import {Select} from "./shared/ui_component/Select.tsx";
+import {IoIosArrowDown, IoIosArrowUp} from "react-icons/io";
 
 function App() {
 
+    const m = [
+        {id: "1", name: "Afganistán"},
+        {id: "2", name: "Albania"},
+        {id: "3", name: "Alemania"},
+        {id: "4", name: "Andorra"},
+        {id: "5", name: "Angola"},
+        {id: "6", name: "Antigua y Barbuda"},
+        {id: "7", name: "Arabia Saudita"},
+        {id: "8", name: "Argelia"},
+        {id: "9", name: "Argentina"},
+        {id: "10", name: "Armenia"},
+        {id: "11", name: "Australia"},
+        {id: "12", name: "Austria"},
+        {id: "13", name: "Azerbaiyán"},
+        {id: "14", name: "Bahamas"},
+        {id: "15", name: "Bangladés"},
+        {id: "16", name: "Barbados"},
+        {id: "17", name: "Baréin"},
+        {id: "18", name: "Bélgica"},
+        {id: "19", name: "Belice"},
+        {id: "20", name: "Benín"},
+        {id: "21", name: "Bielorrusia"},
+        {id: "22", name: "Birmania"},
+        {id: "23", name: "Bolivia"},
+        {id: "24", name: "Bosnia y Herzegovina"},
+        {id: "25", name: "Botsuana"},
+        {id: "26", name: "Brasil"},
+        {id: "27", name: "Brunéi"},
+        {id: "28", name: "Bulgaria"},
+        {id: "29", name: "Burkina Faso"},
+        {id: "30", name: "Burundi"},
+        {id: "31", name: "Bután"}
+
+    ]
   return (
     <>
       <Select
-          leading={
-          <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-               xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 9-7 7-7-7"/>
-          </svg>}
+          callback={(id)=>{
+              console.log(id)
+          }}
+          entries={m}
+          leadingOff={
+          <IoIosArrowDown className="text-2xl" />
+      }
+          leadingOn={
+          <IoIosArrowUp className="text-2xl"/>
+      }
       />
     </>
   )
--- frontend/src/shared/ui_component/Select.tsx
+++ frontend/src/shared/ui_component/Select.tsx
@@ -1,7 +1,10 @@
 import {FC, JSX, useState} from "react";
 
-export const Select:FC<SelectProps> = ({leading}) => {
+export const Select:FC<SelectProps> = ({leadingOn,leadingOff, callback, entries = []}) => {
     const [showList, setShowList] = useState(false)
+    const [itemValue, setItemValue] = useState<string>("Select item");
+    const [leadingValue, setLeadingValue] = useState(false)
+1
 
     const toggleList = () => {
         setShowList(!showList)
@@ -10,17 +13,34 @@ export const Select:FC<SelectProps> = ({leading}) => {
     return (
         <>
         <div className="w-60 h-24 space-y-2 font-semibold">
-                <div className="flex justify-between items-center bg-black text-white p-2 rounded-md"
-                onClick={toggleList}>
-                    selector
-                    {leading}
+                <div className="flex justify-between items-center bg-gray-700 text-white p-2 rounded-md"
+                onClick={() =>{
+                    toggleList(),
+                    setLeadingValue(!leadingValue)}}>
+                    {itemValue}
+                    {leadingValue ? (
+                        <span>
+                            {leadingOn}
+                        </span>
+                    ) :
+                        <span>
+                            {leadingOff}
+                        </span>}
                 </div>
-                {showList ? (
-                    <ul className="bg-black text-white">
-                        <li className="p-2 text-sm hover:bg-terciary-300">
-
-                        </li>
+            {showList ? (
+                <>
+                    <ul className="bg-gray-500 text-white rounded-md overflow-y-auto max-h-32">
+                        {entries.map((entry) => (
+                            <li onClick={()=>{callback(entry.id),
+                                    setItemValue(entry.name),
+                                    toggleList(),
+                                    setLeadingValue(!leadingValue)}}
+                                key={entry.id}
+                                className="p-2 text-sm hover:bg-terciary-300">
+                                {entry.name}
+                            </li>))}
                     </ul>
+                </>
                 ) : null}
         </div>
         </>
@@ -28,6 +48,8 @@ export const Select:FC<SelectProps> = ({leading}) => {
 };
 
 interface SelectProps {
-    leading?: JSX.Element;
-    entries: { id: string, name: string };
+    leadingOn?: JSX.Element;
+    leadingOff?: JSX.Element;
+    entries: { id: string, name: string }[];
+    callback: (id :string)=>void;
 }
